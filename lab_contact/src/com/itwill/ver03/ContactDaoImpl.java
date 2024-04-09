41687cc688de61658d45b2453789a4204526698d
LIMGEONHEE <dlarjsgml321@naver.com>
2024-04-09 10:46:50 +0900

    0409

--- lab_contact/src/com/itwill/ver03/ContactDaoImpl.java
+++ lab_contact/src/com/itwill/ver03/ContactDaoImpl.java
@@ -6,8 +6,9 @@ import java.util.List;
 import com.itwill.ver01.Contact;
 import com.itwill.ver03.ContactDaoImpl;
 
+//controller 클래스
 public class ContactDaoImpl implements ContactDao {
-
+	// ----- singleton
 	private static ContactDaoImpl instance = null;
 
 	private ContactDaoImpl() {
@@ -21,39 +22,52 @@ public class ContactDaoImpl implements ContactDao {
 	}
 
 	private List<Contact> contacts = new ArrayList<>();
-	
 
 	@Override
 	public int create(Contact contact) {
 		contacts.add(contact);
-		return 1;
-		}
 
+		return 1;
+	}
 
 	@Override
 	public List<Contact> read() {
-
 		return contacts;
 	}
 
 	@Override
 	public Contact read(int index) {
-		return contacts.get(index);
+		if (isValidIndex(index)) {
+			return contacts.get(index);
+		} else {
+			return null;
+		}
+
 	}
 
 	@Override
 	public int update(int index, Contact contact) {
-		contacts.set(index, contact);
-		return 1;
+		if (isValidIndex(index)) {
+			contacts.set(index, contact);
+//			contacts.get(index).setName(contact.getName());
+			return 1;
+		} else {
+			return 0;
+		}
 	}
 
 	@Override
 	public int delete(int index) {
-		contacts.remove(index);
-		return 1;
+		if (isValidIndex(index)) {
+			contacts.remove(index);
+			return 1;
+		} else {
+			return 0;
+		}
 	}
 
-	
+	public boolean isValidIndex(int index) {
+		return (index < contacts.size() && index >= 0);
+	}
 
 }
-
