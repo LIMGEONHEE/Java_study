96011ce911a60ad4a691521c15843f495836e7c2
LIMGEONHEE <dlarjsgml321@naver.com>
2024-04-15 16:02:13 +0900

    4/15

--- lab13_inner/src/com/itwill/lambda02/LambdaMain02.java
+++ lab13_inner/src/com/itwill/lambda02/LambdaMain02.java
@@ -9,7 +9,24 @@ interface MyFilter {
 	boolean test(Object x);
 }
 
+@FunctionalInterface
+interface MyMapper {
+    Object transform(Object x);
+}
+
+
+
 public class LambdaMain02 {
+	
+	public List<Object> map(List<Object> list, MyMapper mapper) {
+		List<Object> result = new ArrayList<>();
+		
+		for (Object x : list) {
+			result.add(mapper.transform(x));
+		}
+		
+		return result;
+	}
 	
 	public List<Object> filter(List<Object> list, MyFilter filter) {
 		List<Object> result = new ArrayList<>();
@@ -38,13 +55,29 @@ public class LambdaMain02 {
 		System.out.println(odds);
 		
 		// numbers에서 짝수들만 선택(필터링)
-		List<Object> evens = app.filter(numbers, (x) -> (Integer)x % 2 == 0);
+		List<Object> evens = app.filter(numbers, (x) -> (Integer) x % 2 == 0);
 		System.out.println(evens);
 		
 		List<Object> languages = Arrays.asList("Java", "SQL", "HTML", "JavaScript", "Python");
 		// languages의 원소들 중 문자열의 길이가 5자 이상인 원소들만 선택(필터링)
 		List<Object> longWords = app.filter(languages, (x) -> ((String)x).length() >= 5);
 		System.out.println(longWords);
+		
+		// numbers의 원소들의 제곱을 저장하는 리스트:
+//		List<Object> squares = app.map(numbers, new MyMapper() {
+//			@Override
+//			public Object transform(Object x) {
+//				Integer i = (Integer) x;
+//				return i * i;
+//			}
+//		});
+		List<Object> squares = app.map(numbers, (x) -> (Integer) x * (Integer) x);
+		System.out.println(squares); 
+		
+		// languages의 문자열을 대문자로 변환한 리스트:
+		List<Object> upperCases = app.map(languages, (x) -> ((String) x).toUpperCase());
+		System.out.println(upperCases);
+		
 
 	}
 
---
+++ lab13_inner/src/com/itwill/lambda04/Employee.java
@@ -0,0 +1,67 @@
+package com.itwill.lambda04;
+
+public class Employee {
+	private int id; // 사번
+	private String name; // 이름
+	private String dept; // 부서(department) 이름
+	private String jobTittle; // 직급(사원, 대리, 과장, ...)
+	private double salary; // 급여
+	
+	public Employee(int id, String name, String dept, String jobTittle, double salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.dept = dept;
+		this.jobTittle = jobTittle;
+		this.salary = salary;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDept() {
+		return dept;
+	}
+
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+
+	public String getJobTittle() {
+		return jobTittle;
+	}
+
+	public void setJobTittle(String jobTittle) {
+		this.jobTittle = jobTittle;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", dept=" + dept + ", jobTittle=" + jobTittle + ", salary="
+				+ salary + "]";
+	}
+	
+	
+
+}
