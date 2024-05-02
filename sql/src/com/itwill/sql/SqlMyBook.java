f20985c1373d5be2d8fbe02d93c4814878d1422f
LIMGEONHEE <dlarjsgml321@naver.com>
2024-05-02 20:43:55 +0900

    5/2

---
+++ sql/src/com/itwill/sql/SqlMyBook.java
@@ -0,0 +1,28 @@
+package com.itwill.sql;
+
+public class SqlMyBook {
+	create table mybook (
+		    isbn            number(13) generated always as identity,
+		    title           varchar2(100 char) not null,
+		    writer          varchar2(100 char) not null,
+		    category        varchar2(100 char) not null,
+		    publisher       varchar2(100 char) not null,
+		    bookreview      varchar2(1000 char) not null,   
+		    create_time     timestamp default systimestamp,
+		    modified_time   timestamp default systimestamp,
+		    constraint mybook_pk primary key(isbn)
+		);
+
+		insert into mybook (title, writer, category, publisher, bookreview)
+		values ('각본 없음', '아비 모건', '에세이', '현암사', '재밌다.');
+
+		drop table mybook;
+
+		select * from mybook order by isbn;
+
+		commit;
+	
+	
+	
+	
+}
